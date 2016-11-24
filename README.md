7c89db3a0255d3305f4074aed7e311e99b68017a
Jaime López <jlopez@magento.com>
2016-11-24 19:29:55 +0100

    Adds first version of composer and executor

---
+++ README.md
@@ -0,0 +1,5 @@
+# Testuals
+####Simple PHP unit tests framework
+Testuals is an unit test framework designed to simplify your live allowing you to define some stupid tests in a simple, understandable and more textual way 
+
+[![Minimum PHP Version](https://img.shields.io/badge/php-%3E%3D%205.6-8892BF.svg?style=flat-square)](https://php.net/)
---
+++ src/Command.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * This file is part of Testuals.
+ *
+ * (c) Jaime Lopez <jeims.lopez@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Santa\Testuals;
+
+class Command
+{
+    public function run()
+    {
+        die('Fuck you!' . PHP_EOL);
+    }
+}
